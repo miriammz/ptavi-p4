1b7dcd9632db92d448d2c23dbc694b0fd54523cd
MIRIAM <miriam@ubuntu.(none)>
2014-10-18 15:15:28 +0200

    mandamos 200 ok y guardamos ip y email

--- server.py
+++ server.py
@@ -8,29 +8,35 @@ en UDP simple
 import SocketServer
 import sys
 
-class EchoHandler(SocketServer.DatagramRequestHandler):
+class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
-
+    dicc = {}
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write("Hemos recibido tu peticion" + '\n')
+        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
         print self.client_address
         fichero = open("fichero.txt", "w")
-        IP = self.client_address[0]
-        PUERTO = self.client_address[1]
-        fichero.write(IP + " " + str(PUERTO))
+        ip = self.client_address[0]
+        puerto = self.client_address[1]
+        fichero.write(ip + " " + str(puerto))
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
-            if not line:
+            line2 = line.split(":")
+            self.dicc["cliente"] = line[1]
+            self.dicc["ip"] = ip
+            cliente = self.dicc["cliente"]
+            ip = self.dicc["ip"]
+            if not line or not line2:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
     puerto = int(sys.argv[1])
-    serv = SocketServer.UDPServer(("", puerto), EchoHandler)
+    serv = SocketServer.UDPServer(("", puerto), SIPRegisterHandler)
     print "Lanzado servidor UDP de eco..."
     serv.serve_forever()
