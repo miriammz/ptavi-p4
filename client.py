325f543169ee171e5370299faeb966f6bbd71533
MIRIAM <miriam@ubuntu.(none)>
2014-10-18 15:14:43 +0200

    modificado para enviar register sip

--- client.py
+++ client.py
@@ -12,17 +12,22 @@ import socket
 # Dirección IP del servidor.
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
+register = sys.argv[3]
+email = sys.argv[4]
 
 # Contenido que vamos a enviar
-LINE = sys.argv[3] 
+#LINE = sys.argv[3] 
+LINE = ""
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
-print "Enviado: " + LINE
-my_socket.send(LINE + '\r\n')
+if register == "REGISTER":
+	LINE = "REGISTER sip:" + email + " " + "SIP/1.0\r\n\r\n"
+	print "Enviamos: " + LINE
+my_socket.send(LINE)
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
@@ -32,4 +37,4 @@ print "Terminado socket..."
 my_socket.close()
 print "Fin."
 
-# python client.py ip puerto linea (ej 127.0.0.1 5060 eco eco, soy yo)
+# python client.py ip puerto linea (ej 127.0.0.1 5060 register email@email.com)
