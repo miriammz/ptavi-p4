b5eff4a490ae3f5329f74eced4ee6e5e32059753
Miriam Mendoza Zurita <miriammz@iota43.aulas.gsyc.es>
2014-10-20 10:10:15 +0200

    expires y usage añadidos

--- client.py
+++ client.py
@@ -14,9 +14,9 @@ SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
 register = sys.argv[3]
 email = sys.argv[4]
+expires = int(sys.argv[5])
 
 # Contenido que vamos a enviar
-#LINE = sys.argv[3] 
 LINE = ""
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
@@ -24,10 +24,16 @@ my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
+try:
+except UsageError:
+    print "Usage: client.py ip puerto register sip_addres expires_value
+    sys.exit()
+
 if register == "REGISTER":
-	LINE = "REGISTER sip:" + email + " " + "SIP/1.0\r\n\r\n"
-	print "Enviamos: " + LINE
-my_socket.send(LINE)
+    LINE = "REGISTER sip:" + email + " " + "SIP/1.0\r\n"
+    LINE2 = "Expires: " + expires + "\r\n\r\n"
+    print "Enviamos: " + LINE + LINE2
+my_socket.send(LINE + LINE2 + "\r\n")
 data = my_socket.recv(1024)
 
 print 'Recibido -- ', data
