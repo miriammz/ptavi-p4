f3e831eaddd7eb1bad64be8d856922501b179efd
MIRIAM <miriam@ubuntu.(none)>
2014-10-20 22:34:09 +0200

    variables del bucle modificadas

--- server.py
+++ server.py
@@ -8,42 +8,42 @@ en UDP simple
 import SocketServer
 import sys
 
+
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
     Echo server class
     """
     dicc = {}
+
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write("Hemos recibido tu peticion" + '\n')
-        self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+        #self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
         print self.client_address
         fichero = open("fichero.txt", "w")
         IP = self.client_address[0]
         PUERTO = self.client_address[1]
-        EXPIRES = self.client_address[5]
-        fichero.write(IP + " " + str(PUERTO) + str(EXPIRES))
+        fichero.write(IP + " " + str(PUERTO))
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
             line2 = line.split(":")
-            self.dicc["cliente"] = line[1]
-            self.dicc["ip"] = IP
-            self.dicc["expires"] = EXPIRES
-            cliente = self.dicc["cliente"]
-            IP = self.dicc["ip"]
-            EXPIRES = self.dicc["expires"]
-            if line2[1] == 0:
-            	if cliente in self.dicc:
-            		self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
-            		# borramos al usuario
-            		del self.dicc[cliente]
-            	else:
-            		self.wfile.write("SIP/1.0 410 Gone\r\n\r\n")
+            cliente = line2[0]
+            line3 = line.split("Expires: ")
+            EXPIRES = line3[0]
+            if EXPIRES == 0:
+                if cliente in self.dicc:
+                    self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                    print "22222222222222"
+                    # borramos al usuario
+                    del self.dicc[cliente]
+                else:
+                    self.wfile.write("SIP/1.0 410 Gone\r\n\r\n")
             else:
-            	self.dicc[cliente] = [IP, EXPIRES]
-            	self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                self.dicc[cliente] = [IP, EXPIRES]
+                self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                print "3333333333333333"
             if not line or not line2:
                 break
 
