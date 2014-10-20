00bf52d9f57c417d35af213be1ba4d211a2107c0
Miriam Mendoza Zurita <miriammz@iota43.aulas.gsyc.es>
2014-10-20 10:57:30 +0200

    cliente borrado y expires añadido (modificar)

--- server.py
+++ server.py
@@ -19,24 +19,37 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
         self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
         print self.client_address
         fichero = open("fichero.txt", "w")
-        ip = self.client_address[0]
-        puerto = self.client_address[1]
-        fichero.write(ip + " " + str(puerto))
+        IP = self.client_address[0]
+        PUERTO = self.client_address[1]
+        EXPIRES = self.client_address[5]
+        fichero.write(IP + " " + str(PUERTO) + str(EXPIRES))
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
             print "El cliente nos manda " + line
             line2 = line.split(":")
             self.dicc["cliente"] = line[1]
-            self.dicc["ip"] = ip
+            self.dicc["ip"] = IP
+            self.dicc["expires"] = EXPIRES
             cliente = self.dicc["cliente"]
-            ip = self.dicc["ip"]
+            IP = self.dicc["ip"]
+            EXPIRES = self.dicc["expires"]
+            if line2[1] == 0:
+            	if cliente in self.dicc:
+            		self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+            		# borramos al usuario
+            		del self.dicc[cliente]
+            	else:
+            		self.wfile.write("SIP/1.0 410 Gone\r\n\r\n")
+            else:
+            	self.dicc[cliente] = [IP, EXPIRES]
+            	self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
             if not line or not line2:
                 break
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
-    puerto = int(sys.argv[1])
-    serv = SocketServer.UDPServer(("", puerto), SIPRegisterHandler)
+    PUERTO = int(sys.argv[1])
+    serv = SocketServer.UDPServer(("", PUERTO), SIPRegisterHandler)
     print "Lanzado servidor UDP de eco..."
     serv.serve_forever()
