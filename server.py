7125ec8840d4782a033121f84c495e31e44bd5c9
Miriam Mendoza Zurita <miriammz@iota43.aulas.gsyc.es>
2014-10-24 12:57:47 +0200

    corregido error expires

--- server.py
+++ server.py
@@ -24,25 +24,29 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
         IP = self.client_address[0]
         PUERTO = self.client_address[1]
         fichero.write(IP + " " + str(PUERTO))
+        # Leyendo línea a línea lo que nos envía el cliente
+        line = self.rfile.read()
+        print "El cliente nos manda " + line
+        line = line.split(" ")
+        line2 = line[1].split(":")
+        cliente = line2[1]
+        EXPIRES = line[3]
+       
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print "El cliente nos manda " + line
-            line2 = line.split(":")
-            cliente = line2[0]
-            line3 = line.split("Expires: ")
-            EXPIRES = line3[0]
-            if EXPIRES == 0:
-                if cliente in self.dicc:
-                    self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+            if EXPIRES == '0\r\n\r\n\r\n':
+                
+                if IP in self.dicc:
                     print "22222222222222"
+                    self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
                     # borramos al usuario
                     del self.dicc[cliente]
                 else:
                     self.wfile.write("SIP/1.0 410 Gone\r\n\r\n")
+                    print "11111111111"
             else:
                 self.dicc[cliente] = [IP, EXPIRES]
                 self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
+                #print str(EXPIRES)
                 print "3333333333333333"
             if not line or not line2:
                 break
